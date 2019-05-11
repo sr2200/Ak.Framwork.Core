60bc185617dd2f043426000e2f49340a02ae86a9
Y.A <akf.type.r@gmail.com>
2019-05-11 15:51:36 +0900

    Configクラス追加

---
+++ Akf.Core.Test/AkConfigTest.cs
@@ -0,0 +1,68 @@
+﻿using Akf.Core.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akf.Core.Test
+{
+    [TestClass]
+    public class AkConfigTest
+    {
+        [TestInitialize]
+        public void init()
+        {
+            Console.WriteLine("init");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string jsonFile = "appsettings.json";
+            AkJsonConfig.Init(jsonFile);
+            var conf = AkJsonConfig.Instance;
+            Assert.AreEqual("hogehoge22", conf.GetConfig("section2", "data"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string jsonFile = "appsettings.json";
+            AkJsonConfig.Init(jsonFile);
+            var conf = AkJsonConfig.Instance;
+            Assert.AreEqual("b22", conf.GetConfig("section2", "data2:1"));
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string jsonFile = "appsettings.json";
+            AkJsonConfig.Init(jsonFile);
+            var conf = AkJsonConfig.Instance;
+            Assert.AreEqual("hoge3-1", conf.GetConfig("section3", "subSection1", "data"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string jsonFile = "appsettings.json";
+            AkJsonConfig.Init(jsonFile);
+            var conf = AkJsonConfig.Instance;
+            Assert.AreEqual(10, conf.GetConfig<int>("section1", "data2"));
+        }
+
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string jsonFile = "appsettings.json";
+            AkJsonConfig.Init(jsonFile);
+            var conf = AkJsonConfig.Instance;
+            string[] actual = conf.GetConfigArray("section2", "data2");
+            Assert.AreEqual("a22", actual[0]);
+            Assert.AreEqual("b22", actual[1]);
+            Assert.AreEqual("c22", actual[2]);
+        }
+    }
+}
---
+++ Akf.Core/Config/AkJsonConfig.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Akf.Core.Config
+{
+    public sealed class AkJsonConfig
+    {
+        private static IConfigurationRoot configRoot = null;
+
+        private static bool isInit = false;
+
+        private AkJsonConfig()
+        {
+        }
+
+        private static AkJsonConfig _Instance = null;
+        public static AkJsonConfig Instance
+        {
+            get
+            {
+                if (!isInit)
+                {
+                    throw new ApplicationException("初期化されていません。");
+                }
+                return _Instance;
+            }
+        }
+
+        public static void Init(string filePath)
+        {
+            if (_Instance == null)
+            {
+                _Instance = new AkJsonConfig();
+            }
+            isInit = true;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile(filePath, optional: true);
+            //builder.AddEnvironmentVariables();
+            configRoot = builder.Build();
+        }
+
+        public string GetConfig(string section, string key)
+        {
+            var sec = configRoot.GetSection(section);
+            return sec[key];
+        }
+
+        public T GetConfig<T>(string section, string key)
+        {
+            var sec = configRoot.GetSection(section);
+            string ret = sec[key];
+
+            Type type = typeof(T);
+            if (typeof(int) == type)
+            {
+                return (T)(object)int.Parse(ret);
+            }
+            else if (typeof(short) == type)
+            {
+                return (T)(object)short.Parse(ret);
+            }
+            else if (typeof(long) == type)
+            {
+                return (T)(object)long.Parse(ret);
+            }
+            else if (typeof(double) == type)
+            {
+                return (T)(object)double.Parse(ret);
+            }
+            else if (typeof(float) == type)
+            {
+                return (T)(object)float.Parse(ret);
+            }
+            else if (typeof(decimal) == type)
+            {
+                return (T)(object)decimal.Parse(ret);
+            }
+            else if (typeof(object) == type)
+            {
+                return (T)(object)ret;
+            }
+            else
+            {
+                throw new ApplicationException("対応していない型です。");
+            }
+        }
+
+        public string[] GetConfigArray(string section, string key)
+        {
+            var sec = configRoot.GetSection(section).GetSection(key);
+            return sec.GetChildren().Select(x => x.Value).ToArray();
+        }
+
+        public string GetConfig(string section, string subSection, string key)
+        {
+            var sec = configRoot.GetSection(section);
+            var subSec = sec.GetSection(subSection);
+            return subSec[key];
+        }
+
+    }
+}
